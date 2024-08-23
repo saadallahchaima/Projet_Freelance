7cf063deadc0ec451274a15c209447ab87271de4
chaimjed <shaymaeljed20@gmail.com>
2024-08-23 20:25:40 +0100

    yeddyyy yehdik

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:khedma/screens/onboardings/spalshScreen.dart';
 
 import 'package:khedma/theme/AppTheme.dart';
 
